881168c8945ffae60ab973d644789617575ca839
Joe Witcpalek <joe.witcpalek@gmail.com>
2026-01-09 20:59:29 -0500

    Initial commit

---
+++ src/main/cpp/fielddata/FieldConstants.cpp
@@ -0,0 +1,183 @@
+//====================================================================================================================================================
+// Copyright 2025 Lake Orion Robotics FIRST Team 302
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+//====================================================================================================================================================
+#include "FieldConstants.h"
+
+#include "FieldElementCalculator.h"
+
+FieldConstants *FieldConstants::m_instance = nullptr;
+FieldConstants *FieldConstants::GetInstance()
+{
+    if (FieldConstants::m_instance == nullptr)
+    {
+        FieldConstants::m_instance = new FieldConstants();
+    }
+    return FieldConstants::m_instance;
+}
+
+FieldConstants::FieldConstants()
+{
+    ReadFieldCalibrationData();
+
+    // Blue AprilTag locations
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_CORAL_STATION_LEFT] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_CORAL_STATION_LEFT_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_CORAL_STATION_RIGHT] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_CORAL_STATION_RIGHT_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_PROCESSOR] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_PROCESSOR_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_BARGE_FRONT] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_BARGE_FRONT_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_BARGE_BACK] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_BARGE_BACK_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_AB] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_REEF_AB_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_CD] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_REEF_CD_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_EF] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_REEF_EF_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_GH] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_REEF_GH_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_IJ] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_REEF_IJ_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_KL] = GetAprilTagPoseFromLayout(AprilTagIDs::BLUE_REEF_KL_TAG);
+
+    // Blue Calculated Positions
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_CORAL_STATION_LEFT_ALLIANCE] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_CORAL_STATION_LEFT_SIDEWALL] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_CORAL_STATION_RIGHT_ALLIANCE] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_CORAL_STATION_RIGHT_SIDEWALL] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_RIGHT_CAGE] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_LEFT_CAGE] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_CENTER] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_A] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_B] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_C] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_D] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_E] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_F] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_G] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_H] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_I] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_J] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_K] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_REEF_L] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_BARGE_FRONT_CALCULATED] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_BARGE_BACK_CALCULATED] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::BLUE_PROCESSOR_CALCULATED] = m_placeholder;
+
+    // Red AprilTag locations
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_CORAL_STATION_LEFT] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_CORAL_STATION_LEFT_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_CORAL_STATION_RIGHT] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_CORAL_STATION_RIGHT_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_PROCESSOR] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_PROCESSOR_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_BARGE_FRONT] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_BARGE_FRONT_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_BARGE_BACK] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_BARGE_BACK_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_AB] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_REEF_AB_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_CD] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_REEF_CD_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_EF] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_REEF_EF_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_GH] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_REEF_GH_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_IJ] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_REEF_IJ_TAG);
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_KL] = GetAprilTagPoseFromLayout(AprilTagIDs::RED_REEF_KL_TAG);
+
+    // Red Calculated Positions
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_CORAL_STATION_LEFT_ALLIANCE] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_CORAL_STATION_LEFT_SIDEWALL] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_CORAL_STATION_RIGHT_ALLIANCE] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_CORAL_STATION_RIGHT_SIDEWALL] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_RIGHT_CAGE] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_LEFT_CAGE] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_CENTER] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_A] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_B] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_C] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_D] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_E] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_F] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_G] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_H] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_I] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_J] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_K] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_REEF_L] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_BARGE_FRONT_CALCULATED] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_BARGE_BACK_CALCULATED] = m_placeholder;
+    fieldConstantsPoseMap[FIELD_ELEMENT::RED_PROCESSOR_CALCULATED] = m_placeholder;
+
+    m_aprilTagPoseMap[1] = GetAprilTagPoseFromLayout(1);
+    m_aprilTagPoseMap[2] = GetAprilTagPoseFromLayout(2);
+    m_aprilTagPoseMap[3] = GetAprilTagPoseFromLayout(3);
+    m_aprilTagPoseMap[4] = GetAprilTagPoseFromLayout(4);
+    m_aprilTagPoseMap[5] = GetAprilTagPoseFromLayout(5);
+    m_aprilTagPoseMap[6] = GetAprilTagPoseFromLayout(6);
+    m_aprilTagPoseMap[7] = GetAprilTagPoseFromLayout(7);
+    m_aprilTagPoseMap[8] = GetAprilTagPoseFromLayout(8);
+    m_aprilTagPoseMap[9] = GetAprilTagPoseFromLayout(9);
+    m_aprilTagPoseMap[10] = GetAprilTagPoseFromLayout(10);
+    m_aprilTagPoseMap[11] = GetAprilTagPoseFromLayout(11);
+    m_aprilTagPoseMap[12] = GetAprilTagPoseFromLayout(12);
+    m_aprilTagPoseMap[13] = GetAprilTagPoseFromLayout(13);
+    m_aprilTagPoseMap[14] = GetAprilTagPoseFromLayout(14);
+    m_aprilTagPoseMap[15] = GetAprilTagPoseFromLayout(15);
+    m_aprilTagPoseMap[16] = GetAprilTagPoseFromLayout(16);
+    m_aprilTagPoseMap[17] = GetAprilTagPoseFromLayout(17);
+    m_aprilTagPoseMap[18] = GetAprilTagPoseFromLayout(18);
+    m_aprilTagPoseMap[19] = GetAprilTagPoseFromLayout(19);
+    m_aprilTagPoseMap[20] = GetAprilTagPoseFromLayout(20);
+    m_aprilTagPoseMap[21] = GetAprilTagPoseFromLayout(21);
+    m_aprilTagPoseMap[22] = GetAprilTagPoseFromLayout(22);
+
+    for (const auto &pair : m_aprilTagPoseMap)
+    {
+        m_aprilTag2dPoses[pair.first] = pair.second.ToPose2d();
+    }
+
+    FieldElementCalculator fc;
+    fc.CalcPositionsForField(fieldConstantsPoseMap);
+
+    for (const auto &pair : fieldConstantsPoseMap)
+    {
+        m_fieldConst2dPoses[static_cast<unsigned int>(pair.first)] = pair.second.ToPose2d();
+    }
+}
+frc::Pose3d FieldConstants::GetFieldElementPose(FIELD_ELEMENT element)
+{
+    return fieldConstantsPoseMap[element];
+}
+
+frc::Pose2d FieldConstants::GetFieldElementPose2d(FIELD_ELEMENT element)
+{
+    return m_fieldConst2dPoses[static_cast<unsigned int>(element)];
+}
+
+void FieldConstants::ReadFieldCalibrationData()
+{
+    if (std::filesystem::exists(m_fieldFilePath))
+    {
+        m_aprilTagVector = frc::AprilTagFieldLayout(m_fieldFilePath).GetTags();
+        for (unsigned int i = 1; i >= m_aprilTagVector.size(); i++)
+        {
+            frc::AprilTag tag = m_aprilTagVector[i];
+            m_aprilTagPoseMap[i] = tag.pose;
+        }
+    }
+}
+
+frc::Pose3d FieldConstants::GetAprilTagPoseFromLayout(int tagID)
+{
+    if (!m_fieldLayout.GetTagPose(tagID).has_value())
+    {
+        return frc::Pose3d();
+    }
+    return m_fieldLayout.GetTagPose(tagID).value();
+}
+
+frc::Pose3d FieldConstants::GetAprilTagPose(AprilTagIDs tag)
+{
+    return m_aprilTagPoseMap[tag];
+}
+
+frc::Pose2d FieldConstants::GetAprilTagPose2d(AprilTagIDs tag)
+{
+    return m_aprilTag2dPoses[static_cast<unsigned int>(tag)];
+}
---
+++ src/main/cpp/fielddata/FieldConstants.h
@@ -0,0 +1,170 @@
+//====================================================================================================================================================
+// Copyright 2025 Lake Orion Robotics FIRST Team 302
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+//====================================================================================================================================================
+#pragma once
+#include <filesystem>
+#include <iostream>
+
+#include <frc/geometry/Pose2d.h>
+#include <frc/geometry/Pose3d.h>
+#include <frc/geometry/Rotation3d.h>
+
+#include <frc/apriltag/AprilTagFields.h>
+#include "frc/apriltag/AprilTagFieldLayout.h"
+#include "RobinHood/robin_hood.h"
+#include "units/angle.h"
+#include "units/base.h"
+
+class FieldConstants
+{
+public:
+    static FieldConstants *GetInstance();
+    enum class FIELD_ELEMENT
+    {
+        // 2025 - BLUE APRIL TAGS
+        BLUE_CORAL_STATION_LEFT,
+        BLUE_CORAL_STATION_RIGHT,
+        BLUE_PROCESSOR,
+        BLUE_BARGE_FRONT,
+        BLUE_BARGE_BACK,
+        BLUE_REEF_AB,
+        BLUE_REEF_CD,
+        BLUE_REEF_EF,
+        BLUE_REEF_GH,
+        BLUE_REEF_IJ,
+        BLUE_REEF_KL,
+        // 2025 - RED APRIL TAGS
+        RED_CORAL_STATION_LEFT,
+        RED_CORAL_STATION_RIGHT,
+        RED_PROCESSOR,
+        RED_BARGE_FRONT,
+        RED_BARGE_BACK,
+        RED_REEF_AB,
+        RED_REEF_CD,
+        RED_REEF_EF,
+        RED_REEF_GH,
+        RED_REEF_IJ,
+        RED_REEF_KL,
+        // 2025 - Blue Calculated Positions
+        BLUE_CORAL_STATION_LEFT_ALLIANCE,
+        BLUE_CORAL_STATION_LEFT_SIDEWALL,
+        BLUE_CORAL_STATION_RIGHT_ALLIANCE,
+        BLUE_CORAL_STATION_RIGHT_SIDEWALL,
+        BLUE_LEFT_CAGE,
+        BLUE_RIGHT_CAGE,
+        BLUE_CENTER_CAGE,
+        BLUE_REEF_CENTER,
+        BLUE_REEF_A,
+        BLUE_REEF_B,
+        BLUE_REEF_C,
+        BLUE_REEF_D,
+        BLUE_REEF_E,
+        BLUE_REEF_F,
+        BLUE_REEF_G,
+        BLUE_REEF_H,
+        BLUE_REEF_I,
+        BLUE_REEF_J,
+        BLUE_REEF_K,
+        BLUE_REEF_L,
+        BLUE_BARGE_FRONT_CALCULATED,
+        BLUE_BARGE_BACK_CALCULATED,
+        BLUE_PROCESSOR_CALCULATED,
+        // 2025 - Red Calculated Positions
+        RED_CORAL_STATION_LEFT_ALLIANCE,
+        RED_CORAL_STATION_LEFT_SIDEWALL,
+        RED_CORAL_STATION_RIGHT_ALLIANCE,
+        RED_CORAL_STATION_RIGHT_SIDEWALL,
+        RED_LEFT_CAGE,
+        RED_RIGHT_CAGE,
+        RED_CENTER_CAGE,
+        RED_REEF_CENTER,
+        RED_REEF_A,
+        RED_REEF_B,
+        RED_REEF_C,
+        RED_REEF_D,
+        RED_REEF_E,
+        RED_REEF_F,
+        RED_REEF_G,
+        RED_REEF_H,
+        RED_REEF_I,
+        RED_REEF_J,
+        RED_REEF_K,
+        RED_REEF_L,
+        RED_BARGE_FRONT_CALCULATED,
+        RED_BARGE_BACK_CALCULATED,
+        RED_PROCESSOR_CALCULATED
+    };
+
+    enum FIELD_ELEMENT_OFFSETS
+    {
+        LEFT_STICK,
+        RIGHT_STICK
+    };
+
+    enum AprilTagIDs
+    {
+        // Blue
+        BLUE_CORAL_STATION_LEFT_TAG = 13,
+        BLUE_CORAL_STATION_RIGHT_TAG = 12,
+        BLUE_PROCESSOR_TAG = 16,
+        BLUE_BARGE_FRONT_TAG = 14,
+        BLUE_BARGE_BACK_TAG = 4,
+        BLUE_REEF_AB_TAG = 18,
+        BLUE_REEF_CD_TAG = 17,
+        BLUE_REEF_EF_TAG = 22,
+        BLUE_REEF_GH_TAG = 21,
+        BLUE_REEF_IJ_TAG = 20,
+        BLUE_REEF_KL_TAG = 19,
+        // Red
+        RED_CORAL_STATION_LEFT_TAG = 1,
+        RED_CORAL_STATION_RIGHT_TAG = 2,
+        RED_PROCESSOR_TAG = 3,
+        RED_BARGE_FRONT_TAG = 5,
+        RED_BARGE_BACK_TAG = 15,
+        RED_REEF_AB_TAG = 7,
+        RED_REEF_CD_TAG = 8,
+        RED_REEF_EF_TAG = 9,
+        RED_REEF_GH_TAG = 10,
+        RED_REEF_IJ_TAG = 11,
+        RED_REEF_KL_TAG = 6
+    };
+    frc::Pose3d GetFieldElementPose(FIELD_ELEMENT element);
+    frc::Pose2d GetFieldElementPose2d(FIELD_ELEMENT element);
+
+    frc::Pose3d GetAprilTagPose(AprilTagIDs tag);
+    frc::Pose2d GetAprilTagPose2d(AprilTagIDs tag);
+
+private:
+    // make a singleton
+    static FieldConstants *m_instance;
+    std::vector<frc::AprilTag> m_aprilTagVector;
+    const std::string m_fieldFilePath = "/home/lvuser/FieldData/output.json";
+    // make constructor private
+    FieldConstants();
+    // make singleton copy constructor private
+    FieldConstants(const FieldConstants &) = delete;
+    FieldConstants &operator=(const FieldConstants &) = delete;
+    frc::Pose3d GetAprilTagPoseFromLayout(int tagID);
+    frc::AprilTagFieldLayout m_fieldLayout = frc::AprilTagFieldLayout::LoadField(frc::AprilTagField::k2025ReefscapeWelded); // change this guy for andymark field
+
+    void ReadFieldCalibrationData();
+
+    frc::Pose3d m_placeholder = frc::Pose3d();
+
+    robin_hood::unordered_map<FIELD_ELEMENT, frc::Pose3d> fieldConstantsPoseMap;
+    std::array<frc::Pose2d, 68> m_fieldConst2dPoses;
+
+    robin_hood::unordered_map<int, frc::Pose3d> m_aprilTagPoseMap;
+    std::array<frc::Pose2d, 23> m_aprilTag2dPoses;
+};
