bb6bb3f1ae902f6acfaef1a62830d65e36358e69
alyssaperson1 <alyssa.acker@stu.lok12.org>
2026-02-16 20:48:58 -0500

    I'm attempting to make the autons work

--- src/main/cpp/chassis/commands/TrajectoryDrive.cpp
+++ src/main/cpp/chassis/commands/TrajectoryDrive.cpp
@@ -28,7 +28,7 @@
 #include "chassis/commands/TrajectoryDrive.h"
 #include "utils/logging/debug/Logger.h"
 #include "state/RobotState.h"
-// #include "auton/drivePrimitives/AutonUtils.h" TODO: Uncomment when auton get added back in
+#include "auton/drivePrimitives/AutonUtils.h"
 
 TrajectoryDrive::TrajectoryDrive(
     subsystems::CommandSwerveDrivetrain *chassis) : m_chassis(chassis),
@@ -48,7 +48,7 @@ TrajectoryDrive::TrajectoryDrive(
 
 void TrajectoryDrive::Initialize()
 {
-    // m_trajectory = AutonUtils::GetTrajectoryFromPathFile(m_pathName); TODO: Uncomment when auton get added back in
+    m_trajectory = AutonUtils::GetTrajectoryFromPathFile(m_pathName);
     if (m_trajectory.has_value())
     {
         m_trajectoryStates = m_trajectory.value().samples;
