7861f2c637b6f23320f558f5e9380169dfc3c4b6
Tan560 <97492534+Tan560@users.noreply.github.com>
2026-03-01 13:10:22 -0500

    Neeed to redo launcher

--- src/main/cpp/mechanisms/Launcher/LauncherTuningState.h
+++ src/main/cpp/mechanisms/Launcher/LauncherTuningState.h
@@ -45,7 +45,7 @@ namespace LauncherStates
 		void InitCompBot302();
 		RobotIdentifier m_RobotId;
 
-		units::angular_velocity::revolutions_per_minute_t m_launcherTarget = units::angular_velocity::revolutions_per_minute_t(500);
+		units::angular_velocity::revolutions_per_minute_t m_launcherTarget = units::angular_velocity::revolutions_per_minute_t(1000);
 		const double m_hoodTarget = double(0);
 		const double m_transferTarget = double(0);
 		const double m_turretTarget = double(0);
