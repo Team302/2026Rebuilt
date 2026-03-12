899fcff3e1ef1732652fa9b4dbd7735dbf4ab7fa
Tanay Patel <97492534+Tan560@users.noreply.github.com>
2026-03-12 09:19:49 -0400

    Needs testing on robot and path tuning

--- src/main/cpp/chassis/commands/season_specific_commands/DriveAlongNearestWall.h
+++ src/main/cpp/chassis/commands/season_specific_commands/DriveAlongNearestWall.h
@@ -105,6 +105,6 @@ private:
     /// @brief      Tolerance for Y-coordinate matching when finding closest point on trajectory
     static constexpr units::meter_t m_yDistanceThreshold = 0.25_m;
 
-    static constexpr double m_maxPercentToJoinReversePath = 0.5;
-    static constexpr double m_maxPercentToJoinForwardPath = 0.4;
+    static constexpr double m_maxPercentToJoinReversePath = 0.3;
+    static constexpr double m_maxPercentToJoinForwardPath = 0.5;
 };
