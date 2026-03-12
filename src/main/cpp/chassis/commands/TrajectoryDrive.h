2f8ff161d6a32e96347870c85cd296fd15455c4e
Tanay Patel <97492534+Tan560@users.noreply.github.com>
2026-03-12 09:19:49 -0400

    Needs testing on robot and path tuning

--- src/main/cpp/chassis/commands/TrajectoryDrive.h
+++ src/main/cpp/chassis/commands/TrajectoryDrive.h
@@ -123,7 +123,7 @@ private:
     static constexpr double kIDrive{0.0};
     static constexpr double kDDrive{0.0};
 
-    static constexpr double kPHeading{2.0};
+    static constexpr double kPHeading{6.0};
     static constexpr double kIHeading{0.0};
     static constexpr double kDHeading{0.0};
 
