e0a4578f1a4b749c2ca6d95d2d78a164e082b8d0
Tan560 <97492534+Tan560@users.noreply.github.com>
2026-04-11 10:34:57 -0400

    Revert "gain changes and tables"
    This reverts commit c936f87847fe4f039c91e000fc548ef788743759.

--- src/main/cpp/mechanisms/Launcher/Launcher.cpp
+++ src/main/cpp/mechanisms/Launcher/Launcher.cpp
@@ -593,7 +593,7 @@ void Launcher::InitializeTalonFXSTurretCompBot302()
 	configs.MotorOutput.PeakReverseDutyCycle = -1;
 	configs.MotorOutput.DutyCycleNeutralDeadband = 0;
 
-	configs.MotionMagic.MotionMagicCruiseVelocity = units::angular_velocity::turns_per_second_t(1500);
+	configs.MotionMagic.MotionMagicCruiseVelocity = units::angular_velocity::turns_per_second_t(1000);
 	configs.MotionMagic.MotionMagicAcceleration = units::angular_acceleration::turns_per_second_squared_t(500);
 	configs.MotionMagic.MotionMagicJerk = units::angular_jerk::radians_per_second_cubed_t(0);
 	configs.Commutation.MotorArrangement = MotorArrangementValue::Minion_JST;
--- src/main/cpp/vision/DragonQuest.cpp
+++ src/main/cpp/vision/DragonQuest.cpp
@@ -132,7 +132,7 @@ void DragonQuest::GetEstimatedPose()
     const PoseFrame &latest = frames.back();
     frc::Pose3d robotPose = QuestPoseToRobotPose3d(latest.questPose3d);
 
-    m_isQuestGoofy = false; // PoseUtils::IsPoseJumping(robotPose.ToPose2d(), m_lastCalculatedPose.ToPose2d()) || PoseUtils::IsPoseOffField(robotPose.ToPose2d());
+    m_isQuestGoofy = PoseUtils::IsPoseJumping(robotPose.ToPose2d(), m_lastCalculatedPose.ToPose2d()) || PoseUtils::IsPoseOffField(robotPose.ToPose2d());
 
     m_lastCalculatedPose = robotPose;
     m_lastPoseTimestamp = units::time::second_t{latest.dataTimestamp};
